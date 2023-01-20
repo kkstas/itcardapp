4f82ae315ab788a09939f907ad4881dcf613e54d
Kamil Stasiak <stasiakkamilkrzysztof@gmail.com>
2023-01-21 00:36:06 +0100

    add axios post payload type

--- util/ticketData.ts
+++ util/ticketData.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import { TicketDataType } from '../hooks/asyncStorage';
 
 const BACKEND_URL = 'https://itcarddata-default-rtdb.europe-west1.firebasedatabase.app/';
 
-export async function postTicketData(data) {
+export interface PostTicketDataType extends TicketDataType {
+	email: string | null;
+	firstName: string | null;
+	lastName: string | null;
+	jobTitle: string | null;
+}
+
+export async function postTicketData(data: PostTicketDataType) {
 	const preparedData = JSON.stringify(data);
 	const response = await axios.post(BACKEND_URL + 'tickets.json', preparedData);
 	return response;
