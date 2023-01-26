878a0fed42e3148059c766e2c007d8dfc62f52b1
Kamil Stasiak <stasiakkamilkrzysztof@gmail.com>
2023-01-26 22:50:43 +0100

    moved animations to components folder

--- components/molecules/TicketDataListItem.tsx
+++ components/molecules/TicketDataListItem.tsx
@@ -1,171 +1,181 @@
-import React from 'react';
-import { StyleSheet, View, Dimensions, Alert, TouchableOpacity } from 'react-native';
-import Animated, {
-	useSharedValue,
-	useAnimatedGestureHandler,
-	useAnimatedStyle,
-	withSpring,
-	withTiming,
-	SlideInLeft,
-	SlideInRight,
-	SlideOutRight,
-	SlideOutLeft,
-} from 'react-native-reanimated';
+import React from "react";
 import {
-	PanGestureHandler,
-	PanGestureHandlerGestureEvent,
-} from 'react-native-gesture-handler';
-import { springConfig, timingConfig } from '../../animations/UserDataListAnimationConfig';
+  StyleSheet,
+  View,
+  Dimensions,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+  SlideInLeft,
+  SlideInRight,
+  SlideOutRight,
+  SlideOutLeft,
+} from "react-native-reanimated";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import {
+  springConfig,
+  timingConfig,
+} from "../animations/UserDataListAnimationConfig";
 
-import DeleteListButton from '../atoms/DeleteListButton';
-import useCustomColors from '../../hooks/useCustomColors';
-import { TicketDataType, removeSingleItem } from '../../hooks/asyncStorage';
-import TicketItemContent from '../atoms/TicketItemContent';
+import DeleteListButton from "../atoms/DeleteListButton";
+import useCustomColors from "../../hooks/useCustomColors";
+import { TicketDataType, removeSingleItem } from "../../hooks/asyncStorage";
+import TicketItemContent from "../atoms/TicketItemContent";
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
-const windowDimensions = Dimensions.get('window');
+const windowDimensions = Dimensions.get("window");
 const BUTTON_WIDTH = 80;
 const MAX_TRANSLATE = -BUTTON_WIDTH;
 
 type ListItemProps = {
-	item: TicketDataType;
-	fromLeft: boolean;
-	index: number;
+  item: TicketDataType;
+  fromLeft: boolean;
+  index: number;
 };
-export default function TicketDataListItem({ item, fromLeft, index }: ListItemProps) {
-	const isRemoving = useSharedValue(false);
-	const translateX = useSharedValue(0);
-	const t = useCustomColors();
-	const navigation = useNavigation();
+export default function TicketDataListItem({
+  item,
+  fromLeft,
+  index,
+}: ListItemProps) {
+  const isRemoving = useSharedValue(false);
+  const translateX = useSharedValue(0);
+  const t = useCustomColors();
+  const navigation = useNavigation();
 
-	type AnimatedGHContext = {
-		startX: number;
-	};
-	const handler = useAnimatedGestureHandler<
-		PanGestureHandlerGestureEvent,
-		AnimatedGHContext
-	>({
-		onStart: (_evt, ctx) => {
-			ctx.startX = translateX.value;
-		},
+  type AnimatedGHContext = {
+    startX: number;
+  };
+  const handler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    AnimatedGHContext
+  >({
+    onStart: (_evt, ctx) => {
+      ctx.startX = translateX.value;
+    },
 
-		onActive: (evt, ctx) => {
-			const nextTranslate = evt.translationX + ctx.startX;
-			translateX.value = Math.min(0, Math.max(nextTranslate, MAX_TRANSLATE));
-		},
+    onActive: (evt, ctx) => {
+      const nextTranslate = evt.translationX + ctx.startX;
+      translateX.value = Math.min(0, Math.max(nextTranslate, MAX_TRANSLATE));
+    },
 
-		onEnd: (evt) => {
-			if (evt.velocityX < -20) {
-				translateX.value = withSpring(MAX_TRANSLATE, springConfig(evt.velocityX));
-			} else {
-				translateX.value = withSpring(0, springConfig(evt.velocityX));
-			}
-		},
-	});
+    onEnd: (evt) => {
+      if (evt.velocityX < -20) {
+        translateX.value = withSpring(
+          MAX_TRANSLATE,
+          springConfig(evt.velocityX)
+        );
+      } else {
+        translateX.value = withSpring(0, springConfig(evt.velocityX));
+      }
+    },
+  });
 
-	const stylesAnimation = useAnimatedStyle(() => {
-		if (isRemoving.value) {
-			return {
-				height: withTiming(0, timingConfig),
-				transform: [
-					{
-						translateX: withTiming(-windowDimensions.width, timingConfig),
-					},
-				],
-			};
-		}
+  const stylesAnimation = useAnimatedStyle(() => {
+    if (isRemoving.value) {
+      return {
+        height: withTiming(0, timingConfig),
+        transform: [
+          {
+            translateX: withTiming(-windowDimensions.width, timingConfig),
+          },
+        ],
+      };
+    }
 
-		return {
-			height: 83,
-			transform: [
-				{
-					translateX: translateX.value,
-				},
-			],
-		};
-	});
+    return {
+      height: 83,
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+      ],
+    };
+  });
 
-	function handleRemove() {
-		Alert.alert(
-			'Czy napewno chcesz usunąć ten element?',
-			'Po usunięciu nie będzie można go przywrócić',
-			[
-				{
-					text: 'Usuń',
-					onPress: () => {
-						removeSingleItem(item.id);
-						isRemoving.value = true;
-					},
-					style: 'destructive',
-				},
-				{
-					text: 'Anuluj',
-					style: 'cancel',
-					onPress: () => (translateX.value = withTiming(0, timingConfig)),
-				},
-			]
-		);
-	}
+  function handleRemove() {
+    Alert.alert(
+      "Czy napewno chcesz usunąć ten element?",
+      "Po usunięciu nie będzie można go przywrócić",
+      [
+        {
+          text: "Usuń",
+          onPress: () => {
+            removeSingleItem(item.id);
+            isRemoving.value = true;
+          },
+          style: "destructive",
+        },
+        {
+          text: "Anuluj",
+          style: "cancel",
+          onPress: () => (translateX.value = withTiming(0, timingConfig)),
+        },
+      ]
+    );
+  }
 
-	const removeButton = {
-		title: 'Usuń',
-		backgroundColor: t.pink,
-		color: 'white',
-		onPress: handleRemove,
-		windowWidth: windowDimensions.width,
-		BUTTON_WIDTH: BUTTON_WIDTH,
-	};
+  const removeButton = {
+    title: "Usuń",
+    backgroundColor: t.pink,
+    color: "white",
+    onPress: handleRemove,
+    windowWidth: windowDimensions.width,
+    BUTTON_WIDTH: BUTTON_WIDTH,
+  };
 
-	const removeButtonColor = useAnimatedStyle(() => {
-		return {
-			opacity: translateX.value === 0 ? 0 : 1,
-		};
-	});
+  const removeButtonColor = useAnimatedStyle(() => {
+    return {
+      opacity: translateX.value === 0 ? 0 : 1,
+    };
+  });
 
-	const goToTicketModal = () => {
-		if (translateX.value === 0) {
-			navigation.navigate('TicketModal', { data: item });
-		}
-	};
+  const goToTicketModal = () => {
+    if (translateX.value === 0) {
+      navigation.navigate("TicketModal", { data: item });
+    }
+  };
 
-	return (
-		<TouchableOpacity onPress={goToTicketModal}>
-			<Animated.View
-				style={styles.item}
-				entering={(fromLeft ? SlideInLeft : SlideInRight).delay(index * 50)}
-				exiting={(fromLeft ? SlideOutLeft : SlideOutRight).delay(index * 50)}
-			>
-				<PanGestureHandler
-					activeOffsetX={[-10, 10]}
-					onGestureEvent={handler}
-				>
-					<Animated.View style={stylesAnimation}>
-						<TicketItemContent
-							goToTicketModal={goToTicketModal}
-							item={item}
-						/>
+  return (
+    <TouchableOpacity onPress={goToTicketModal}>
+      <Animated.View
+        style={styles.item}
+        entering={(fromLeft ? SlideInLeft : SlideInRight).delay(index * 50)}
+        exiting={(fromLeft ? SlideOutLeft : SlideOutRight).delay(index * 50)}
+      >
+        <PanGestureHandler activeOffsetX={[-10, 10]} onGestureEvent={handler}>
+          <Animated.View style={stylesAnimation}>
+            <TicketItemContent goToTicketModal={goToTicketModal} item={item} />
 
-						<Animated.View style={[styles.buttonsContainer, removeButtonColor]}>
-							<DeleteListButton item={removeButton} />
-						</Animated.View>
-					</Animated.View>
-				</PanGestureHandler>
-			</Animated.View>
-		</TouchableOpacity>
-	);
+            <Animated.View style={[styles.buttonsContainer, removeButtonColor]}>
+              <DeleteListButton item={removeButton} />
+            </Animated.View>
+          </Animated.View>
+        </PanGestureHandler>
+      </Animated.View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-	item: {
-		justifyContent: 'center',
-	},
+  item: {
+    justifyContent: "center",
+  },
 
-	buttonsContainer: {
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: windowDimensions.width,
-		width: windowDimensions.width,
-	},
+  buttonsContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: windowDimensions.width,
+    width: windowDimensions.width,
+  },
 });
--- components/molecules/UserDataListItem.tsx
+++ components/molecules/UserDataListItem.tsx
@@ -1,146 +1,160 @@
-import React from 'react';
-import { StyleSheet, View, Dimensions, Alert } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Dimensions, Alert } from "react-native";
 import Animated, {
-	useSharedValue,
-	useAnimatedGestureHandler,
-	useAnimatedStyle,
-	withSpring,
-	withTiming,
-	SlideInLeft,
-	SlideInRight,
-	SlideOutRight,
-	SlideOutLeft,
-} from 'react-native-reanimated';
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+  SlideInLeft,
+  SlideInRight,
+  SlideOutRight,
+  SlideOutLeft,
+} from "react-native-reanimated";
 import {
-	PanGestureHandler,
-	PanGestureHandlerGestureEvent,
-} from 'react-native-gesture-handler';
-import { springConfig, timingConfig } from '../../animations/UserDataListAnimationConfig';
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import {
+  springConfig,
+  timingConfig,
+} from "../animations/UserDataListAnimationConfig";
 
-import DeleteListButton from '../atoms/DeleteListButton';
-import ListItemContent, { Data } from '../atoms/ListItemContent';
-import useCustomColors from '../../hooks/useCustomColors';
+import DeleteListButton from "../atoms/DeleteListButton";
+import ListItemContent, { Data } from "../atoms/ListItemContent";
+import useCustomColors from "../../hooks/useCustomColors";
 
-const windowDimensions = Dimensions.get('window');
+const windowDimensions = Dimensions.get("window");
 const BUTTON_WIDTH = 80;
 const MAX_TRANSLATE = -BUTTON_WIDTH;
 
 type ListItemProps = {
-	item: Data;
-	fromLeft: boolean;
-	index: number;
+  item: Data;
+  fromLeft: boolean;
+  index: number;
 };
-export default function UserDataListItem({ item, fromLeft, index }: ListItemProps) {
-	const isRemoving = useSharedValue(false);
-	const translateX = useSharedValue(0);
-	const t = useCustomColors();
+export default function UserDataListItem({
+  item,
+  fromLeft,
+  index,
+}: ListItemProps) {
+  const isRemoving = useSharedValue(false);
+  const translateX = useSharedValue(0);
+  const t = useCustomColors();
 
-	type AnimatedGHContext = {
-		startX: number;
-	};
-	const handler = useAnimatedGestureHandler<
-		PanGestureHandlerGestureEvent,
-		AnimatedGHContext
-	>({
-		onStart: (_evt, ctx) => {
-			ctx.startX = translateX.value;
-		},
+  type AnimatedGHContext = {
+    startX: number;
+  };
+  const handler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    AnimatedGHContext
+  >({
+    onStart: (_evt, ctx) => {
+      ctx.startX = translateX.value;
+    },
 
-		onActive: (evt, ctx) => {
-			const nextTranslate = evt.translationX + ctx.startX;
-			translateX.value = Math.min(0, Math.max(nextTranslate, MAX_TRANSLATE));
-		},
+    onActive: (evt, ctx) => {
+      const nextTranslate = evt.translationX + ctx.startX;
+      translateX.value = Math.min(0, Math.max(nextTranslate, MAX_TRANSLATE));
+    },
 
-		onEnd: (evt) => {
-			if (evt.velocityX < -20) {
-				translateX.value = withSpring(MAX_TRANSLATE, springConfig(evt.velocityX));
-			} else {
-				translateX.value = withSpring(0, springConfig(evt.velocityX));
-			}
-		},
-	});
+    onEnd: (evt) => {
+      if (evt.velocityX < -20) {
+        translateX.value = withSpring(
+          MAX_TRANSLATE,
+          springConfig(evt.velocityX)
+        );
+      } else {
+        translateX.value = withSpring(0, springConfig(evt.velocityX));
+      }
+    },
+  });
 
-	const stylesAnimation = useAnimatedStyle(() => {
-		if (isRemoving.value) {
-			return {
-				height: withTiming(0, timingConfig),
-				transform: [
-					{
-						translateX: withTiming(-windowDimensions.width, timingConfig),
-					},
-				],
-			};
-		}
+  const stylesAnimation = useAnimatedStyle(() => {
+    if (isRemoving.value) {
+      return {
+        height: withTiming(0, timingConfig),
+        transform: [
+          {
+            translateX: withTiming(-windowDimensions.width, timingConfig),
+          },
+        ],
+      };
+    }
 
-		return {
-			height: 78,
-			transform: [
-				{
-					translateX: translateX.value,
-				},
-			],
-		};
-	});
+    return {
+      height: 78,
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+      ],
+    };
+  });
 
-	function handleRemove() {
-		Alert.alert(
-			'Czy napewno chcesz usunąć ten element?',
-			'Po usunięciu nie będzie można go przywrócić',
-			[
-				{ text: 'Usuń', onPress: () => (isRemoving.value = true), style: 'destructive' },
-				{
-					text: 'Anuluj',
-					style: 'cancel',
-					onPress: () => (translateX.value = withTiming(0, timingConfig)),
-				},
-			]
-		);
-	}
+  function handleRemove() {
+    Alert.alert(
+      "Czy napewno chcesz usunąć ten element?",
+      "Po usunięciu nie będzie można go przywrócić",
+      [
+        {
+          text: "Usuń",
+          onPress: () => (isRemoving.value = true),
+          style: "destructive",
+        },
+        {
+          text: "Anuluj",
+          style: "cancel",
+          onPress: () => (translateX.value = withTiming(0, timingConfig)),
+        },
+      ]
+    );
+  }
 
-	const removeButton = {
-		title: 'Usuń',
-		backgroundColor: t.pink,
-		color: 'white',
-		onPress: handleRemove,
-		windowWidth: windowDimensions.width,
-		BUTTON_WIDTH: BUTTON_WIDTH,
-	};
+  const removeButton = {
+    title: "Usuń",
+    backgroundColor: t.pink,
+    color: "white",
+    onPress: handleRemove,
+    windowWidth: windowDimensions.width,
+    BUTTON_WIDTH: BUTTON_WIDTH,
+  };
 
-	const removeButtonColor = useAnimatedStyle(() => {
-		return {
-			opacity: translateX.value === 0 ? 0 : 1,
-		};
-	});
+  const removeButtonColor = useAnimatedStyle(() => {
+    return {
+      opacity: translateX.value === 0 ? 0 : 1,
+    };
+  });
 
-	return (
-		<Animated.View
-			style={styles.item}
-			entering={(fromLeft ? SlideInLeft : SlideInRight).delay(index * 50)}
-			exiting={(fromLeft ? SlideOutLeft : SlideOutRight).delay(index * 50)}
-		>
-			<PanGestureHandler activeOffsetX={[-10, 10]} onGestureEvent={handler}>
-				<Animated.View style={stylesAnimation}>
-					<ListItemContent item={item} />
+  return (
+    <Animated.View
+      style={styles.item}
+      entering={(fromLeft ? SlideInLeft : SlideInRight).delay(index * 50)}
+      exiting={(fromLeft ? SlideOutLeft : SlideOutRight).delay(index * 50)}
+    >
+      <PanGestureHandler activeOffsetX={[-10, 10]} onGestureEvent={handler}>
+        <Animated.View style={stylesAnimation}>
+          <ListItemContent item={item} />
 
-					<Animated.View style={[styles.buttonsContainer, removeButtonColor]}>
-						<DeleteListButton item={removeButton} />
-					</Animated.View>
-				</Animated.View>
-			</PanGestureHandler>
-		</Animated.View>
-	);
+          <Animated.View style={[styles.buttonsContainer, removeButtonColor]}>
+            <DeleteListButton item={removeButton} />
+          </Animated.View>
+        </Animated.View>
+      </PanGestureHandler>
+    </Animated.View>
+  );
 }
 
 const styles = StyleSheet.create({
-	item: {
-		justifyContent: 'center',
-	},
+  item: {
+    justifyContent: "center",
+  },
 
-	buttonsContainer: {
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: windowDimensions.width,
-		width: windowDimensions.width,
-	},
+  buttonsContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: windowDimensions.width,
+    width: windowDimensions.width,
+  },
 });
