d9270d52355515503f784a6cae0afbe79e3b6d0a
Kamil Stasiak <stasiakkamilkrzysztof@gmail.com>
2023-01-27 00:47:36 +0100

    locating buttons animations

--- components/atoms/LoadingOverlay.tsx
+++ components/atoms/LoadingOverlay.tsx
@@ -1,31 +1,33 @@
-import Animated, { FadeInDown, FadeOutUp } from 'react-native-reanimated';
-import { StyleSheet, Text } from 'react-native';
-import useCustomColors from '../../hooks/useCustomColors';
+import Animated, { FadeInDown, FadeOutUp } from "react-native-reanimated";
+import { StyleSheet, Text } from "react-native";
+import useCustomColors from "../../hooks/useCustomColors";
 
 export default function LoadingOverlay({
-	paddingBottom,
-	fontSize,
+  paddingBottom,
+  fontSize,
 }: {
-	paddingBottom?: number;
-	fontSize?: number;
+  paddingBottom?: number;
+  fontSize?: number;
 }) {
-	const t = useCustomColors();
-	const fSize = fontSize || 22;
-	return (
-		<Animated.View
-			entering={FadeInDown}
-			exiting={FadeOutUp}
-			style={[styles.container, { paddingBottom: paddingBottom || 0 }]}
-		>
-			<Text style={[styles.text, { fontSize: fSize, color: t.tint }]}>Ładowanie...</Text>
-		</Animated.View>
-	);
+  const t = useCustomColors();
+  const fSize = fontSize || 22;
+  return (
+    <Animated.View
+      entering={FadeInDown}
+      exiting={FadeOutUp}
+      style={[styles.container, { paddingBottom: paddingBottom || 0 }]}
+    >
+      <Text style={[styles.text, { fontSize: fSize, color: t.tint }]}>
+        Ładowanie...
+      </Text>
+    </Animated.View>
+  );
 }
 
 const styles = StyleSheet.create({
-	text: {},
-	container: {
-		position: 'absolute',
-		zIndex: 10,
-	},
+  text: {},
+  container: {
+    position: "absolute",
+    zIndex: 10,
+  },
 });
