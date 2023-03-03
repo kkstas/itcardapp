a0051824af1c4fdb8f6f78feef09f8883e3cde7d
Kamil Stasiak <stasiakkamilkrzysztof@gmail.com>
2023-03-03 17:05:15 +0100

    scroll to top on focus and on second tab bar press in tabtwoscreen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,42 +1,42 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import { Pressable } from "react-native";
-import ModalScreen from "../screens/ModalScreen";
-import NotFoundScreen from "../screens/NotFoundScreen";
-import TabOneScreen from "../screens/TabOneScreen";
-import TabTwoScreen from "../screens/TabTwoScreen";
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { Pressable } from 'react-native';
+import ModalScreen from '../screens/ModalScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import TabOneScreen from '../screens/TabOneScreen';
+import TabTwoScreen from '../screens/TabTwoScreen';
 import {
   RootStackParamList,
   RootTabParamList,
   RootTabScreenProps,
   TabTwoMainStackParamList,
-} from "../types";
-import TabThreeScreen from "../screens/TabThreeScreen";
-import { CustomLightTheme, CustomDarkTheme } from "../constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
-import HeaderButton from "../components/common/HeaderButton";
-import LoginScreen from "../screens/LoginScreen";
-import InfoModalScreen from "../screens/InfoModalScreen";
-import CreateTicketScreen from "../screens/CreateTicketScreen";
-import ScanReceiptScreen from "../screens/ScanReceiptScreen";
-import LocateATMScreen from "../screens/LocateATMScreen";
-import { useAppSelector, useAppDispatch } from "../hooks/reduxHooks";
-import { logOut } from "../store/slices/userInfo";
-import useCustomColors from "../hooks/useCustomColors";
-import { StatusBar } from "expo-status-bar";
-import TicketModalScreen from "../screens/TicketModalScreen";
-import MapScreen from "../screens/MapScreen";
-import { HeaderBackgroundBlur } from "./HeaderBackgroundBlur";
-import ReceiptModalScreen from "../screens/ReceiptModalScreen";
-import { clearUserData } from "../util/rememberMe";
-import { getThemePreference } from "../features/userPreferences/asyncStorageHandler";
-import { setThemeState } from "../features/userPreferences/userPreferencesSlice";
+} from '../types';
+import TabThreeScreen from '../screens/TabThreeScreen';
+import { CustomLightTheme, CustomDarkTheme } from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import HeaderButton from '../components/common/HeaderButton';
+import LoginScreen from '../screens/LoginScreen';
+import InfoModalScreen from '../screens/InfoModalScreen';
+import CreateTicketScreen from '../screens/CreateTicketScreen';
+import ScanReceiptScreen from '../screens/ScanReceiptScreen';
+import LocateATMScreen from '../screens/LocateATMScreen';
+import { useAppSelector, useAppDispatch } from '../hooks/reduxHooks';
+import { logOut } from '../store/slices/userInfo';
+import useCustomColors from '../hooks/useCustomColors';
+import { StatusBar } from 'expo-status-bar';
+import TicketModalScreen from '../screens/TicketModalScreen';
+import MapScreen from '../screens/MapScreen';
+import { HeaderBackgroundBlur } from './HeaderBackgroundBlur';
+import ReceiptModalScreen from '../screens/ReceiptModalScreen';
+import { clearUserData } from '../util/rememberMe';
+import { getThemePreference } from '../features/userPreferences/asyncStorageHandler';
+import { setThemeState } from '../features/userPreferences/userPreferencesSlice';
 
 const CLTheme = {
   ...DefaultTheme,
@@ -51,9 +51,9 @@ const setThemeFromAsyncToRedux = async () => {
   const dispatch = useAppDispatch();
   const themeFromAsync = await getThemePreference();
   if (
-    themeFromAsync === "dark" ||
-    themeFromAsync === "default" ||
-    themeFromAsync === "light"
+    themeFromAsync === 'dark' ||
+    themeFromAsync === 'default' ||
+    themeFromAsync === 'light'
   ) {
     dispatch(setThemeState({ theme: themeFromAsync }));
   }
@@ -74,9 +74,9 @@ export default function Navigation() {
     <>
       <StatusBar
         animated={true}
-        style={t.theme === "light" ? "dark" : "light"}
+        style={t.theme === 'light' ? 'dark' : 'light'}
       />
-      <NavigationContainer theme={t.theme === "dark" ? CDTheme : CLTheme}>
+      <NavigationContainer theme={t.theme === 'dark' ? CDTheme : CLTheme}>
         <RootNavigator />
       </NavigationContainer>
     </>
@@ -93,55 +93,55 @@ function RootNavigator() {
     <Stack.Navigator>
       {!isLoggedIn ? (
         <Stack.Screen
-          name="LoginScreen"
+          name='LoginScreen'
           component={LoginScreen}
           options={{ headerShown: false }}
         />
       ) : (
         <>
           <Stack.Screen
-            name="Root"
+            name='Root'
             component={BottomTabNavigator}
             options={{ headerShown: false }}
           />
           <Stack.Screen
-            name="NotFound"
+            name='NotFound'
             component={NotFoundScreen}
-            options={{ title: "Oops!" }}
+            options={{ title: 'Oops!' }}
           />
           <Stack.Group
             screenOptions={{
-              presentation: "modal",
-              contentStyle: { backgroundColor: "#4040400" },
+              presentation: 'modal',
+              contentStyle: { backgroundColor: '#4040400' },
             }}
           >
             <Stack.Screen
-              name="Modal"
+              name='Modal'
               component={ModalScreen}
-              options={{ title: "Informacje" }}
+              options={{ title: 'Informacje' }}
             />
             <Stack.Screen
-              name="InfoModal"
+              name='InfoModal'
               component={InfoModalScreen}
               options={{
                 headerShown: false,
-                presentation: "modal",
+                presentation: 'modal',
               }}
             />
             <Stack.Screen
-              name="TicketModal"
+              name='TicketModal'
               component={TicketModalScreen}
               options={{
                 headerShown: false,
-                presentation: "modal",
+                presentation: 'modal',
               }}
             />
             <Stack.Screen
-              name="ReceiptModal"
+              name='ReceiptModal'
               component={ReceiptModalScreen}
               options={{
                 headerShown: false,
-                presentation: "modal",
+                presentation: 'modal',
               }}
             />
           </Stack.Group>
@@ -165,21 +165,21 @@ function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabTwo"
+      initialRouteName='TabTwo'
       screenOptions={{
         tabBarActiveTintColor: t.tint,
       }}
     >
       <BottomTab.Screen
-        name="TabOne"
+        name='TabOne'
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           headerTransparent: true,
           headerBackground: () => <HeaderBackgroundBlur />,
-          title: "Dokumenty",
+          title: 'Dokumenty',
           tabBarIcon: ({ color }) => (
             <Ionicons
-              name="layers-outline"
+              name='layers-outline'
               size={28}
               color={color}
               style={{ marginBottom: -3 }}
@@ -187,13 +187,13 @@ function BottomTabNavigator() {
           ),
           headerLeft: () => (
             <Pressable
-              onPress={() => navigation.navigate("Modal")}
+              onPress={() => navigation.navigate('Modal')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
               })}
             >
               <Ionicons
-                name="information-circle-outline"
+                name='information-circle-outline'
                 size={26}
                 color={t.tintTapState}
                 style={{ marginLeft: 15 }}
@@ -203,15 +203,15 @@ function BottomTabNavigator() {
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
+        name='TabTwo'
         component={TabTwoMainStackNavigator}
         options={{
           headerShown: false,
-          title: "Ekran główny",
+          title: 'Ekran główny',
           tabBarIcon: ({ color }) => (
             <Ionicons
               size={28}
-              name="home-outline"
+              name='home-outline'
               color={color}
               style={{ marginBottom: -3 }}
             />
@@ -219,32 +219,32 @@ function BottomTabNavigator() {
         }}
       />
       <BottomTab.Screen
-        name="TabThree"
+        name='TabThree'
         component={TabThreeScreen}
-        options={({ navigation }: RootTabScreenProps<"TabThree">) => ({
+        options={({ navigation }: RootTabScreenProps<'TabThree'>) => ({
           headerTransparent: true,
           headerBackground: () => <HeaderBackgroundBlur />,
           headerLeft: () => (
             <HeaderButton
-              text="Ekran główny"
-              position="left"
-              icon="chevron-back-outline"
-              onPress={() => navigation.navigate("TabTwo")}
+              text='Ekran główny'
+              position='left'
+              icon='chevron-back-outline'
+              onPress={() => navigation.navigate('TabTwo')}
             />
           ),
           headerRight: () => (
             <HeaderButton
               onPress={logOutHandler}
-              text="Wyloguj "
-              position="right"
-              icon="log-out-outline"
+              text='Wyloguj '
+              position='right'
+              icon='log-out-outline'
             />
           ),
-          title: "Profil",
+          title: 'Profil',
           tabBarIcon: ({ color }) => (
             <Ionicons
               style={{ marginBottom: -3, marginRight: 1 }}
-              name="person-outline"
+              name='person-outline'
               size={28}
               color={color}
             />
@@ -259,38 +259,38 @@ const TabTwoMainStack = createNativeStackNavigator<TabTwoMainStackParamList>();
 
 function TabTwoMainStackNavigator() {
   return (
-    <TabTwoMainStack.Navigator initialRouteName="TabTwoScreen">
+    <TabTwoMainStack.Navigator initialRouteName='TabTwoScreen'>
       <TabTwoMainStack.Screen
-        name="TabTwoScreen"
+        name='TabTwoScreen'
         component={TabTwoScreen}
         options={{ headerShown: false }}
       />
       <TabTwoMainStack.Screen
-        name="CreateTicketScreen"
-        options={{ title: "Ekran zgłoszenia", headerBackTitle: "Powrót" }}
+        name='CreateTicketScreen'
+        options={{ title: 'Ekran zgłoszenia', headerBackTitle: 'Powrót' }}
         component={CreateTicketScreen}
       />
       <TabTwoMainStack.Screen
-        name="ScanReceiptScreen"
+        name='ScanReceiptScreen'
         component={ScanReceiptScreen}
-        options={{ title: "Skanuj potwierdzenie", headerBackTitle: "Powrót" }}
+        options={{ title: 'Skanuj potwierdzenie', headerBackTitle: 'Powrót' }}
       />
       <TabTwoMainStack.Screen
-        name="LocateATMScreen"
+        name='LocateATMScreen'
         options={{
-          title: "Znajdź bankomat",
-          headerBackTitle: "Powrót",
+          title: 'Znajdź bankomat',
+          headerBackTitle: 'Powrót',
           headerTransparent: true,
           headerBackground: () => <HeaderBackgroundBlur />,
         }}
         component={LocateATMScreen}
       />
       <TabTwoMainStack.Screen
-        name="MapScreen"
+        name='MapScreen'
         component={MapScreen}
         options={{
-          title: "Wybierz lokalizację",
-          headerBackTitle: "Powrót",
+          title: 'Wybierz lokalizację',
+          headerBackTitle: 'Powrót',
           headerTransparent: true,
           headerBackground: () => <HeaderBackgroundBlur />,
         }}
